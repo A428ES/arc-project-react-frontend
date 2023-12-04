93d92c25282d5917651ed4210bc72b8ac8657570
FIRST_NAME LAST_NAME <estrada.serg@gmail.com>
2023-12-04 10:45:07 -0600

    added no network connectivity checker

--- frontend/src/utility/requester.js
+++ frontend/src/utility/requester.js
@@ -3,51 +3,49 @@ import { useState } from "react";
 export default function HTTPRequester() {
   const [dataFeed, setData] = useState(null);
   const [errorFeed, setError] = useState(null);
-  let errorOccured = false;
 
-  const submitRequest = (
+  const submitRequest = async (
     path = "",
     req_method = "POST",
     arg_body = {},
     arg_headers = null
   ) => {
-    let request_structure = {
-      method: req_method,
-    };
-
-    if (arg_headers == null) {
-      request_structure.headers = {
-        "Content-Type": "application/json",
+    try {
+      let request_structure = {
+        method: req_method,
       };
 
-      if (localStorage.getItem("user_token")) {
-        request_structure.headers.Authorization =
-          "Bearer " + localStorage.getItem("user_token");
+      if (arg_headers == null) {
+        request_structure.headers = {
+          "Content-Type": "application/json",
+        };
+
+        if (localStorage.getItem("user_token")) {
+          request_structure.headers.Authorization =
+            "Bearer " + localStorage.getItem("user_token");
+        }
       }
+
+      if (req_method === "POST") {
+        request_structure.body = JSON.stringify(arg_body);
+      }
+
+      const response = await fetch(
+        `http://${process.env.REACT_APP_HOST}:5000/${path}`,
+        request_structure
+      );
+
+      if (!response.ok) {
+        const errorData = await response.json();
+        setError(errorData.error || "An HTTP error occurred");
+      } else {
+        const data = await response.json();
+        setData(data);
+        setError(null);
+      }
+    } catch (error) {
+      setError("A network error occurred");
     }
-
-    if (req_method === "POST") {
-      request_structure.body = JSON.stringify(arg_body);
-    }
-
-    fetch(
-      `http://${process.env.REACT_APP_HOST}:5000/${path}`,
-      request_structure
-    )
-      .then((response) => {
-        if (!response.ok) {
-          errorOccured = true;
-        }
-
-        return response.json();
-      })
-      .then((data) => {
-        if (errorOccured === false) {
-          setData(data);
-        } else {
-          setError(data.error);
-        }
-      });
   };
 
   return { dataFeed, errorFeed, submitRequest };
