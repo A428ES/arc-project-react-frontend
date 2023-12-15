4b80c5038e2b091ae063b58bdf9c4dba5e388e4b
FIRST_NAME LAST_NAME <estrada.serg@gmail.com>
2023-12-15 12:32:58 -0600

    simplified network request logic

--- frontend/src/components/comments/CommentDropDown.js
+++ frontend/src/components/comments/CommentDropDown.js
@@ -1,48 +1,56 @@
 import { useEffect, useState, useContext } from "react";
 import CommentDisplay from "./CommentsDisplay";
-import HTTPRequester from "../../utility/requester";
+import HTTPRequester from "../../utility/Requester";
 import { AuthContext } from "../../context/UserContext";
 
 export default function CommentDropDown(prop) {
-  const [authState, setAuthState] = useContext(AuthContext);
-  const [commentTotal, setCommentTotal] = useState(0);
+  const [authState] = useContext(AuthContext);
   const [viewingComment, setView] = useState(false);
-  const [newComment, setNewComment] = useState();
-  const { dataFeed, errorFeed, submitRequest: getData } = HTTPRequester();
+  const [submissions, setSubmissions] = useState(null);
+  const httpRequester = HTTPRequester();
+
+  const requestRefresh = () => {
+    httpRequester.submitRequest("comments/display", "POST", {
+      story_id: prop.storyID,
+    });
+  };
+
+  const getCommentCount = () => {
+    if (Array.isArray(submissions?.results)) {
+      return submissions.results.length;
+    }
+
+    return 0;
+  };
 
   useEffect(() => {
-    if (dataFeed === null || newComment === true) {
-      getData(`comments/count?id=${prop.storyID}`, "GET");
-      setNewComment(false);
+    if (!Array.isArray(httpRequester.dataFeed?.results)) {
+      requestRefresh();
     } else {
-      setCommentTotal(dataFeed.results);
+      setSubmissions(httpRequester.dataFeed);
     }
-  }, [dataFeed, newComment]);
-
-  let movePage = () => {
-    setView(true);
-  };
+  }, [httpRequester.dataFeed]);
 
   return (
     <>
       {viewingComment === false ? (
         <header className="articleHeader" id="p2">
-          <a href="javascript:void(0)" onClick={() => movePage()}>
-            comments ({commentTotal})
+          <a href="javascript:void(0)" onClick={() => setView(true)}>
+            comments ({getCommentCount()})
           </a>
         </header>
       ) : (
         <>
           <header className="articleHeader" id="p2">
             <a href="javascript:void(0)" onClick={() => setView(false)}>
-              Close {commentTotal} comments
+              Close {getCommentCount()} comments
             </a>
           </header>
           <CommentDisplay
-            viewType="comments/display"
+            key={prop.storyID}
+            httpRequester={httpRequester}
+            submissions={submissions}
             storyID={prop.storyID}
-            setNew={setNewComment}
-            newComment={newComment}
             authorUUID={
               authState.userData ? authState.userData.uuid : undefined
             }
