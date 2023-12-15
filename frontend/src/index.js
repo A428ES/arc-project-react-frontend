b6825326b2cd5eede6290dcb7d76dd0b3283a5af
FIRST_NAME LAST_NAME <estrada.serg@gmail.com>
2023-12-15 12:32:18 -0600

    updated context path

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { AuthContextProvider } from "../src/context/user_context";
+import { AuthContextProvider } from "../src/context/UserContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
