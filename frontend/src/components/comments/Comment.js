9db75d0c68df27164ee98aee2926e88f7d5f62e4
FIRST_NAME LAST_NAME <estrada.serg@gmail.com>
2023-12-15 12:32:38 -0600

    created comment component

---
+++ frontend/src/components/comments/Comment.js
@@ -0,0 +1,28 @@
+import { useContext } from "react";
+import { AuthContext } from "../../context/UserContext";
+import CommentOwnerBar from "./CommentOwnerBar";
+
+export default function Comment({ item, httpRequester }) {
+  const [authState] = useContext(AuthContext);
+
+  return (
+    <>
+      <section>
+        {" "}
+        <header className="articleHeader" id="p1">
+          {item.author} said the following on {item.date}
+        </header>
+        <p class="article">{item.content}</p>
+        {authState.userData &&
+        authState.userData["uuid"] === item.author_uuid ? (
+          <CommentOwnerBar
+            commentUUID={item.comment_uuid}
+            httpRequester={httpRequester}
+          />
+        ) : (
+          <></>
+        )}
+      </section>
+    </>
+  );
+}
