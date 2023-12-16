fb49b715c99b79c54b79eafc78281ad5a519ebb8
FIRST_NAME LAST_NAME <estrada.serg@gmail.com>
2023-12-16 07:41:38 -0600

    expanded comment display

--- frontend/src/components/comments/CommentsDisplay.js
+++ frontend/src/components/comments/CommentsDisplay.js
@@ -2,33 +2,66 @@ import { useEffect, useState, useContext } from "react";
 import { AuthContext } from "../../context/UserContext";
 import NewComment from "./NewComment";
 import Comment from "./Comment";
+import HTTPRequester from "../../utility/Requester";
 
 export default function CommentDisplay(prop) {
   const [authState] = useContext(AuthContext);
-  const httpRequester = prop.httpRequester;
-  const submissions = prop.submissions;
+  const [submissions, setSubmissions] = useState(null);
+  const httpRequester = HTTPRequester();
+  let inView = prop?.viewing;
+
+  if (inView === undefined) {
+    inView = true;
+  }
+
+  const requestRefresh = () => {
+    httpRequester.submitRequest(prop.viewType, "POST", {
+      story_id: prop.storyID,
+    });
+  };
+
+  useEffect(() => {
+    if (!Array.isArray(httpRequester.dataFeed?.results)) {
+      requestRefresh();
+    } else {
+      if (prop?.extraState !== undefined) {
+        prop.extraState(httpRequester.dataFeed);
+      }
+
+      setSubmissions(httpRequester.dataFeed);
+    }
+  }, [httpRequester.dataFeed]);
 
   return (
     <>
       <section>
-        {submissions.results && submissions.results !== "none" ? (
+        {Array.isArray(submissions?.results) ? (
           submissions.results.map((item) => (
-            <Comment
-              key={item.comment_uuid}
-              httpRequester={httpRequester}
-              item={item}
-            />
+            <>
+              {inView ? (
+                <Comment
+                  key={item.comment_uuid}
+                  httpRequester={httpRequester}
+                  item={item}
+                />
+              ) : (
+                <></>
+              )}
+            </>
           ))
         ) : (
           <p class="article">No comments to view</p>
         )}
-
-        <NewComment
-          key={prop.storyID}
-          storyID={prop.storyID}
-          httpRequester={httpRequester}
-          loggedIn={authState.userLoggedIn}
-        />
+        {inView && prop.viewType !== "comments/mycomments" ? (
+          <NewComment
+            key={prop.storyID}
+            storyID={prop.storyID}
+            httpRequester={httpRequester}
+            loggedIn={authState.userLoggedIn}
+          />
+        ) : (
+          <></>
+        )}
       </section>
     </>
   );
