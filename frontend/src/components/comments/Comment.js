e91d9928091e5e837460bcc9304b919dc3ec2b3a
FIRST_NAME LAST_NAME <estrada.serg@gmail.com>
2023-12-20 10:30:10 -0600

    improved comment component

--- frontend/src/components/comments/Comment.js
+++ frontend/src/components/comments/Comment.js
@@ -6,23 +6,17 @@ export default function Comment({ item, httpRequester }) {
   const [authState] = useContext(AuthContext);
 
   return (
-    <>
-      <section>
-        {" "}
-        <header className="articleHeader" id="p1">
-          {item.author} said the following on {item.date}
-        </header>
-        <p class="article">{item.content}</p>
-        {authState.userData &&
-        authState.userData["uuid"] === item.author_uuid ? (
-          <CommentOwnerBar
-            commentUUID={item.comment_uuid}
-            httpRequester={httpRequester}
-          />
-        ) : (
-          <></>
-        )}
-      </section>
-    </>
+    <section>
+      <header className="articleHeader" id="p1">
+        {item.author} said the following on {item.date}
+      </header>
+      <p className="article">{item.content}</p>
+      {authState.userData?.uuid === item.author_uuid && (
+        <CommentOwnerBar
+          commentUUID={item.comment_uuid}
+          httpRequester={httpRequester}
+        />
+      )}
+    </section>
   );
 }
