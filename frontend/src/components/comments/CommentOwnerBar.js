31204ba2cf0ffa174980e4274d0421fa5ecd4f81
FIRST_NAME LAST_NAME <estrada.serg@gmail.com>
2023-12-20 10:30:31 -0600

    improved commentownerbar component

--- frontend/src/components/comments/CommentOwnerBar.js
+++ frontend/src/components/comments/CommentOwnerBar.js
@@ -3,30 +3,29 @@ import DeleteComment from "./DeleteComment";
 
 export default function CommentOwnerBar({ httpRequester, commentUUID }) {
   const [isActive, setActive] = useState(false);
-  const [modContext, setMod] = useState("");
+  const [modContext, setModContext] = useState("");
 
   let handleSubmit = () => {
     setActive(true);
-    setMod(
+    setModContext(
       <DeleteComment
         setActive={setActive}
         httpRequester={httpRequester}
         commentUUID={commentUUID}
-        key={commentUUID}
       />
     );
   };
+
   return (
     <>
-      {isActive === false ? (
+      {!isActive && (
         <header className="articleHeader" id="p1">
-          <a href="javascript:void(0)" onClick={() => handleSubmit()}>
+          <button onClick={handleSubmit} className="link-button">
             Delete Comment
-          </a>
+          </button>
         </header>
-      ) : (
-        <>{modContext}</>
       )}
+      {isActive && modContext}
     </>
   );
 }
