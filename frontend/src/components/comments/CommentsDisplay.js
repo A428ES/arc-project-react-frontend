af0c7bc8a7f69a9a7226f037f4172b5c5f4fd17b
FIRST_NAME LAST_NAME <estrada.serg@gmail.com>
2023-12-21 06:00:03 -0600

    reduced commentsdisplay to caller for comment component

--- frontend/src/components/comments/CommentsDisplay.js
+++ frontend/src/components/comments/CommentsDisplay.js
@@ -1,63 +1,14 @@
-import { useEffect, useState, useContext } from "react";
-import { AuthContext } from "../../context/UserContext";
-import NewComment from "./NewComment";
 import Comment from "./Comment";
-import HTTPRequester from "../../utility/Requester";
 
-export default function CommentDisplay(prop) {
-  const [authState] = useContext(AuthContext);
-  const [submissions, setSubmissions] = useState(null);
-  const httpRequester = HTTPRequester();
-  let inView = prop?.viewing === undefined ? true : prop?.viewing;
-
-  const requestRefresh = () => {
-    httpRequester.submitRequest(prop.viewType, "POST", {
-      story_id: prop.storyID,
-    });
-  };
-
-  useEffect(() => {
-    if (!Array.isArray(httpRequester.dataFeed?.results)) {
-      requestRefresh();
-    } else {
-      if (prop?.extraState !== undefined) {
-        prop.extraState(httpRequester.dataFeed);
-      }
-
-      setSubmissions(httpRequester.dataFeed);
-    }
-  }, [httpRequester.dataFeed]);
-
+export default function CommentDisplay({ item, httpRequester }) {
   return (
     <>
       <section>
-        {Array.isArray(submissions?.results) ? (
-          submissions.results.map((item) => (
-            <>
-              {inView ? (
-                <Comment
-                  key={item.comment_uuid}
-                  httpRequester={httpRequester}
-                  item={item}
-                />
-              ) : (
-                <></>
-              )}
-            </>
-          ))
-        ) : (
-          <p class="article">No comments to view</p>
-        )}
-        {inView && prop.viewType !== "comments/mycomments" ? (
-          <NewComment
-            key={prop.storyID}
-            storyID={prop.storyID}
-            httpRequester={httpRequester}
-            loggedIn={authState.userLoggedIn}
-          />
-        ) : (
-          <></>
-        )}
+        <Comment
+          key={item.comment_uuid}
+          httpRequester={httpRequester}
+          item={item}
+        />
       </section>
     </>
   );
