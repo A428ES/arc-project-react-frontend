d9d75be1f943acae240cd7878261e7ab43874377
FIRST_NAME LAST_NAME <estrada.serg@gmail.com>
2023-12-22 14:01:06 -0600

    created new pop up component

---
+++ frontend/src/components/layout/Popup.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const PopupEditor = ({ item, onSave, onClose, componentToRender }) => {
+  const [editedItem, setEditedItem] = React.useState(item);
+
+  const handleChange = (event) => {
+    setEditedItem({ ...editedItem, [event.target.name]: event.target.value });
+  };
+
+  const handleSave = () => {
+    onSave(editedItem);
+    onClose();
+  };
+
+  return (
+    <div className="popup-editor">
+      <div className="popup-content">
+        {componentToRender && componentToRender()}
+      </div>
+    </div>
+  );
+};
+
+export default PopupEditor;
